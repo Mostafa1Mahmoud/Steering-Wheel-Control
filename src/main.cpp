e2e7b25c91bcbbdeae0819e256e5f4ba1e78ae97
Mostafa Mahmoud <mmossttafa@gmail.com>
2021-04-28 23:07:00 +0200

    Starting Commit

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+
+  bool Direction = 0, Reset = 0;
+  int Counter, Slots, Pulses = 0 , Resistance , Radiuse;
+  double Voltage, Current, Speed , time = 0.0; 
+  char directionPin = 13 , counterPin = 12, decoderPin = 8, resetPin = 7, voltagePin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(directionPin ,INPUT);
+  pinMode(counterPin   ,INPUT);
+  pinMode(decoderPin   ,INPUT);
+  pinMode(resetPin     ,INPUT);
+  pinMode(voltagePin   ,INPUT);
+   
+}
+
+double calcSpeed(){
+    if(Pulses == Slots){
+      time -= millis();
+      Pulses %= Slots;
+      Speed = (2*PI*Radiuse)/time;
+    }
+    return Speed;
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // the Direction of the wheel
+  Direction = digitalRead(directionPin);
+  
+  // the reset button
+  Reset     = digitalRead(resetPin);
+  
+  // the lab button
+  Counter  += digitalRead(counterPin);
+  
+  // the number of bulses for calculating the speed
+  Pulses   += digitalRead(decoderPin);
+
+  // the voltage & current come from the battery
+  Voltage   = analogRead(voltagePin);
+  Current   = Voltage / Resistance;
+
+  calcSpeed();
+
+
+
+  
+
+}
