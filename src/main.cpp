9d2e9e7c8165ed89548d2f6fb5b3b08b6a7b99ee
Mostafa Mahmoud <47725230+Mostafa1Mahmoud@users.noreply.github.com>
2022-02-24 07:59:13 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,72 @@
 #include <Arduino.h>
+#include "MPU9250.h"
 
-  bool Direction = 0, Reset = 0;
-  int Counter, Slots, leftPulses = 0 , rightPulses = 0 , Resistance , Radiuse , totalLaps;
-  double Voltage, Current, leftSpeed = 0.0 , rightSpeed = 0.0,Time = 0.0,usedCurrent = 0.0, SOC = 0.0, TotalCharge = 0;
-  unsigned long currentTime, lastTime , Interval = 0, socInterval = 1, socCurrentTime = 0, socLastTime = 0;
-  char directionPin = 13 , counterPin = 12, leftEncoderPin = 2,rightEncoderPin = 3, resetPin = 7, voltagePin = A0;
+MPU9250 mpu;
+bool Direction = 0, Reset = 0;
+int Counter, Slots, leftPulses = 0 , rightPulses = 0 , Resistance , Radiuse , totalLaps;
+double Voltage, Current, leftSpeed = 0.0 , rightSpeed = 0.0,Time = 0.0,usedCurrent = 0.0, SOC = 0.0, TotalCharge = 0;
+double accelerationX = 0.0, accelerationY = 0.0, accelerationZ = 0.0, gyroX = 0.0,gyroY = 0.0, gyroZ = 0.0;
+double accelerationBiasX = 0.0, accelerationBiasY = 0.0, accelerationBiasZ = 0.0, gyroBiasX = 0.0, gyroBiasY = 0.0, gyroBiasZ = 0.0;
+double magBiasX = 0.0, magBiasY = 0.0, magBiasZ = 0.0, magX = 0.0, magY = 0.0, magZ = 0.0, yaw = 0.0, pitch = 0.0, roll = 0.0;
+unsigned long currentTime, lastTime , Interval = 0, socInterval = 1, socCurrentTime = 0, socLastTime = 0;
+char directionPin = 13 , counterPin = 12, leftEncoderPin = 2,rightEncoderPin = 3, resetPin = 7, voltagePin = A0;
 
+void mpuCalibrate(){
+  Wire.begin();
+  delay(2000);
+  Serial.println("Accel Gyro calibration will start in 5sec.");
+  Serial.println("Please leave the device still on the flat plane.");
+  mpu.verbose(true);
+  delay(5000);
+  mpu.calibrateAccelGyro();
+  Serial.println("Mag calibration will start in 5sec.");
+  Serial.println("Please Wave device in a figure eight until done.");
+  delay(5000);
+  mpu.calibrateMag();
+  print_calibration();
+  mpu.verbose(false);
+}
+
+void calAccelerationBias(){
+  accelerationBiasX = mpu.getAccBiasX() * 1000.lf / (double)MPU9250::CALIB_ACCEL_SENSITIVITY;
+  accelerationBiasY = mpu.getAccBiasY() * 1000.lf / (double)MPU9250::CALIB_ACCEL_SENSITIVITY;
+  accelerationBiasZ = mpu.getAccBiasZ() * 1000.lf / (double)MPU9250::CALIB_ACCEL_SENSITIVITY;
+}
+void calAcceleration(){
+  accelerationX = getAccX();
+  accelerationY = getAccY();
+  accelerationZ = getAccZ();
+}
+
+void calGyroBias(){
+  mpu.update_accel_gyro();
+  gyroBiasX = mpu.getGyroBiasX() / (double)MPU9250::CALIB_GYRO_SENSITIVITY;
+  gyroBiasY = mpu.getGyroBiasY() / (double)MPU9250::CALIB_GYRO_SENSITIVITY;
+  gyroBiasZ = mpu.getGyroBiasZ() / (double)MPU9250::CALIB_GYRO_SENSITIVITY;
+}
+
+void calGyro(){
+  mpu.update_accel_gyro();
+  gyroX = getGyroX();
+  gyroY = getGyroY();
+  gyroZ = getGyroZ();
+}
+void calMagBias(){
+  magBiasX = getMagBiasX();
+  magBiasY = getMagBiasY();
+  magBiasZ = getMagBiasZ();
+}
+void calMag(){
+  update_mag();
+  magX = getMagX();
+  magY = getMagY();
+  magZ = getMagZ();
+}
+void calRollPitchYaw() {
+    yaw = mpu.getYaw();
+    pitch = mpu.getPitch();
+    roll = mpu.getRoll();
+}
 void setup() {
   // put your setup code here, to run once:
   pinMode(directionPin     ,INPUT);
